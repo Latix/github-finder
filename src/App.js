e6bcd6f6de6accd1871082f35b34e9fe6463815d
Kamsi Kodi <kamsikodi@gmail.com>
2020-04-23 00:18:12 +0100

    Added Not Found Page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import NavBar from './components/layouts/NavBar';
-import Users from './components/Users/Users';
 import User from './components/Users/User';
-import Search from './components/Users/Search';
 import Alert from './components/layouts/Alert';
+import Home from './components/pages/Home';
 import About from './components/pages/About';
 import GithubState from './context/github/GithubState';
 import AlertState from './context/alert/AlertState';
+import NotFound from './components/pages/NotFound';
+
 import './App.css';
 
 const App = () => {
@@ -20,14 +21,10 @@ const App = () => {
             <div className='container'>
                 <Alert />
                 <Switch>
-                  <Route exact path='/' render={ props => (
-                    <Fragment>
-                      <Search />
-                        <Users />
-                    </Fragment>
-                  )} />
+                  <Route exact path='/' component={Home} />
                   <Route exact path='/about' component={About} />
                   <Route exact path='/user/:login' component={User} />
+                  <Route component={NotFound} />
                 </Switch>
             </div>
         </div>
